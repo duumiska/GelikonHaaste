37aebdf363533475855e99a621b742737a2a90d6
Aki Korpua <aki.korpua@gmail.com>
2018-04-22 12:57:01 +0300

    Eka talteen

---
+++ Tekstiseikkailu/Pelimoottori.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tekstiseikkailu
+{
+    public class Pelimoottori
+    {
+        public static void KirjoitaRuuudulle(string teksti)
+        {
+            System.Console.WriteLine(teksti);
+        }
+
+    }
+}
